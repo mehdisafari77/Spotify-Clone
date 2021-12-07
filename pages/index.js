a6189e84d334f562193eed351001ee0a7eda7ca2
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-07 14:20:04 -0800

    added initial desigininng of sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-
-      <h1>This is a test</h1>
-
+    <div className="">
       <main>
-        {/* sidebar */}
+        <Sidebar />
         {/* center */}
 
         {/* sidebar */}
