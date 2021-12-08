97a12537e4219f40b5e9f84c8c8da4ba09f2ff0d
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-08 06:34:27 -0800

    testing tailwind, was broken

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,21 +7,20 @@ import {
     RssIcon,
 } from '@heroicons/react/outline'
 
-import { signOut, useSession } from 'next-auth/react'
+// import { signOut, useSession } from 'next-auth/react'
 
 
 function Sidebar() {
-    const { data: session, status } = useSession()
-    console.log(session)
+    // const { data: session, status } = useSession()
+    // console.log(session)
     return (
         <div className='text-gray-500 p-5 text-sm border-r-gray-900'>
             <div className='space-y-4'>
-                <button 
+                {/* <button 
                     className='flex items-center space-x-2 hover:text-white'
-                    onClick={() => signOut()}
                 >
                     <p>Logout</p>
-                </button>
+                </button> */}
                 <button className='flex items-center space-x-2 hover:text-white'>
                     <HomeIcon className='h-5 w-5' />
                     <p>Home</p>
