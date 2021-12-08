e1d0738515564a0ea85d724b599cf25ccf107f8c
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-07 22:13:22 -0800

    import spotfiy webapi and necessary scopes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,9 +4,10 @@ import SpotifyProvider from 'next-auth/providers/spotify'
 export default NextAuth({
     providers: [
         SpotifyProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-            // authorization
+            clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+            clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+            authorization: LOGIN_URL,
         }),
+        // ...add more providers
     ],
 })
