be999635ae861f4fc11acc2213f04aef3a09ff69
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-12-07 23:03:48 -0800

    done with Spotify Login

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,29 @@
-function Login() {
-    return (
-        <div>
-            <h1>login</h1>
+import { getProviders, signIn } from "next-auth/react";
+
+function Login({ providers }) {
+  return (
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="p-5 bg-[#18D860] rounded-full text-white"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default Login
+export default Login;
+
+// This is the recommended way for Next.js 9.3 or newer
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
